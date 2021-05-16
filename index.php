24a82aa38b688ab3efa95b553c2184daccb723bb
Carlos Fabian Palacios <carlosfabian92001@gmail.com>
2021-05-15 19:40:02 -0500

    cambia el titulo de minuscula a mayuscula

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div class="container">
-            <a class="navbar-brand px-1" id="logo-menu" href="#">INFOTEC</a>
+            <a class="navbar-brand px-1" id="logo-menu" href="#">infotec</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
